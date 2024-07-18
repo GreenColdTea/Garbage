f64a90fff5662ece51d2783d14c1d96c2f69b9b5
Edger228 <isakov.jasurbek9@gmail.com>
2024-07-19 03:23:43 +0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -135,7 +135,7 @@ class Main extends Sprite
 		
 		SUtil.doTheCheck();
 	
-		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
