32251c8d4d801467c4d4db302563b02ca7394fd3
Edger228 <isakov.jasurbek9@gmail.com>
2024-07-20 15:37:15 +0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -97,6 +97,12 @@ class Main extends Sprite
 
 	public function new()
 	{
+           FlxG.save.bind("MyGameSave");
+           if (FlxG.save.data.exeInfoShown == null) {
+               FlxG.save.data.exeInfoShown = false;
+               FlxG.save.flush();
+	   } 
+		
 		super();
 
                 SUtil.gameCrashCheck();
