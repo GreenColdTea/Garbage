6dc8ef306e95bebd65361a03b5939f16c878ed52
Edger228 <isakov.jasurbek9@gmail.com>
2024-07-20 07:15:32 +0500

    Update EXEInfoState.hx

--- source/EXEInfoState.hx
+++ source/EXEInfoState.hx
@@ -22,42 +22,41 @@ using StringTools;
 
 class EXEInfoState extends MusicBeatState
 {
-
-	var sonicWarn:FlxSprite;
+   var sonicWarn:FlxSprite;
    var discl:FlxSprite;
    private var Confirm:FlxSound;
 
-	override function create()
-	{
-      discl = new FlxSprite().loadGraphic(Paths.image('warnings/disclaimer/main'));
-		discl.screenCenter();
-		discl.setGraphicSize(1280, 720);
-      discl.alpha = 0;
-		add(discl);
+   override function create()
+   {
+        discl = new FlxSprite().loadGraphic(Paths.image('warnings/disclaimer/main'));
+	discl.screenCenter();
+	discl.setGraphicSize(1280, 720);
+        discl.alpha = 0;
+	add(discl);
  
-      Confirm = FlxG.sound.play(Paths.sound('confirmMenu'));
+        Confirm = FlxG.sound.play(Paths.sound('confirmMenu'));
 
-		sonicWarn = new FlxSprite(-100, -600);
-		sonicWarn.frames = Paths.getSparrowAtlas('warnings/disclaimer/sonic');
-		sonicWarn.animation.addByPrefix('idle', "idle", 22);
-		sonicWarn.animation.play('idle');
-		sonicWarn.alpha = 0;
-		sonicWarn.scale.x = 2;
-		sonicWarn.scale.y = 2;
-		sonicWarn.antialiasing = true;
-		sonicWarn.updateHitbox();
-      add(sonicWarn);
+	sonicWarn = new FlxSprite(-100, -600);
+	sonicWarn.frames = Paths.getSparrowAtlas('warnings/disclaimer/sonic');
+	sonicWarn.animation.addByPrefix('idle', "idle", 22);
+	sonicWarn.animation.play('idle');
+	sonicWarn.alpha = 0;
+	sonicWarn.scale.x = 2;
+	sonicWarn.scale.y = 2;
+	sonicWarn.antialiasing = true;
+	sonicWarn.updateHitbox();
+        add(sonicWarn);
 
-		 #if windows
-		    DiscordClient.changePresence("In the Disclaimer Menu", null);
-		 #end
+        #if windows
+	    DiscordClient.changePresence("In the Disclaimer Menu", null);
+        #end
 
-      FlxTween.tween(sonicWarn, {alpha: 1}, 2.25);
-      FlxTween.tween(discl, {alpha: 1}, 2.25);
+       FlxTween.tween(sonicWarn, {alpha: 1}, 2.25);
+       FlxTween.tween(discl, {alpha: 1}, 2.25);
 
-      #if android
-         addVirtualPad(NONE, A);
-      #end
+       #if android
+          addVirtualPad(NONE, A);
+       #end
 
 		super.create();
 	}
