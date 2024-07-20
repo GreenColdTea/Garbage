07e8cab66ee913ebf5863c2423e724f16591d429
Edger228 <isakov.jasurbek9@gmail.com>
2024-07-20 07:48:27 +0500

    Update Intro.hx

--- source/Intro.hx
+++ source/Intro.hx
@@ -46,23 +46,10 @@ class Intro extends MusicBeatState
       var video = new VideoHandler();
       var video2 = new VideoHandler();
       video.canSkip = false;
-      video2.canSkip = false;
+      video2.canSkip = true;
       video.finishCallback = function()
       {
          video2.playVideo(Paths.video('sonicexe-intro'));
-	 #if android
-		for (touch in FlxG.touches.list)
-		{
-			if (touch.justPressed)
-			{
-				pressedEnter = true;
-			}
-		}
-	 #end
-	 if (FlxG.keys.justPressed.Enter)
-	 {
-		MusicBeatState.switchState(new TitleState())
-	 }
       }
       video2.finishCallback = function()
       {
