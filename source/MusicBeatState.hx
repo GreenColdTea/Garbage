9b8d4bd9d7b7510246afd142517bbecb4e4a1eb9
Edger228 <isakov.jasurbek9@gmail.com>
2024-07-22 23:13:17 +0500

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -14,6 +14,7 @@ import flixel.util.FlxGradient;
 import flixel.FlxState;
 import flixel.FlxCamera;
 import flixel.FlxBasic;
+import flixel.input.mouse.FlxMouse
 
 #if android
 import flixel.input.actions.FlxActionInput;
