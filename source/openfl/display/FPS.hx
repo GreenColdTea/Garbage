ea7192223069c665330995ecd43a9c0804af8df1
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-07-29 09:12:01 +0500

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -47,7 +47,7 @@ class FPS extends TextField
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
-		defaultTextFormat = new TextFormat(Assets.getFont(Paths.font("sonic-cd-menu-font.ttf")).fontName, 15, color);
+		defaultTextFormat = new TextFormat(Assets.getFont(Paths.font("sonic-cd-menu-font.ttf")).fontName, 16, color);
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
